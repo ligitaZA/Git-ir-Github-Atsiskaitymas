69a414cc0f92f90e09c4e07978f1ad060de553dd
ligitaZA <ligitazaicevaite@gmail.com>
2023-01-09 16:13:38 +0200

    fixing commits

--- main.js
+++
@@ -1,9 +0,0 @@
-function openModal() { 
-  document.getElementById('myModal').style.display = 'block';
-  document.getElementById('display').style.display = 'none';
-}
-
-function closeModal() {
-  document.getElementById('myModal').style.display = 'none';
-  document.getElementById('display').style.display = 'block';
-}
