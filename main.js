3c3a6993540ad22cf0d0e2a65163b699d81315c7
ligitaZA <ligitazaicevaite@gmail.com>
2023-01-09 11:47:45 +0200

    added js to intro modal

---
+++ main.js
@@ -0,0 +1,9 @@
+function openModal() { 
+  document.getElementById('myModal').style.display = 'block';
+  document.getElementById('display').style.display = 'none';
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = 'none';
+  document.getElementById('display').style.display = 'block';
+}
