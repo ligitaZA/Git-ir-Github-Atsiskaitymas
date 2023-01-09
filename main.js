3ea75c7197aab7eab8618251264daaeed7c7c848
ligitaZA <ligitazaicevaite@gmail.com>
2023-01-09 14:25:25 +0200

    Revert "added other modals, styles and js"
    This reverts commit c666a8c6354aca98035e4122e4410c1c89f13c3d.

--- main.js
+++ main.js
@@ -1,46 +1,9 @@
-// pirmas 
-function firstModal() { 
+function openModal() { 
   document.getElementById('myModal').style.display = 'block';
   document.getElementById('display').style.display = 'none';
 }
 
-function closeFirstModal() {
+function closeModal() {
   document.getElementById('myModal').style.display = 'none';
-  document.getElementById('display').style.display = 'block';
-}
-
-
-//antras
-function secondModal() { 
-  document.getElementById('workModal').style.display = 'block';
-  document.getElementById('display').style.display = 'none';
-}
-
-function closeSecondModal() {
-  document.getElementById('workModal').style.display = 'none';
-  document.getElementById('display').style.display = 'block';
-}
-
-
-//trecias
-function thirdModal() { 
-  document.getElementById('aboutModal').style.display = 'block';
-  document.getElementById('display').style.display = 'none';
-}
-
-function closeThirdModal() {
-  document.getElementById('aboutModal').style.display = 'none';
-  document.getElementById('display').style.display = 'block';
-}
-
-
-//ketvirtas
-function fourthModal() { 
-  document.getElementById('contactModal').style.display = 'block';
-  document.getElementById('display').style.display = 'none';
-}
-
-function closeFourthModal() {
-  document.getElementById('contactModal').style.display = 'none';
   document.getElementById('display').style.display = 'block';
 }
